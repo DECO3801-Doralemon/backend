84492e07b2fbeb643867743966f7acf2ca8a6c46
Figo Muhammad <figomuhammad9@protonmail.com>
2021-08-14 21:55:55 +0700

    Fix profile feature

--- profile_feature/views.py
+++ profile_feature/views.py
@@ -1,23 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from .models import AvoidedFood, WasteStat, Customer
 from django.http import JsonResponse
+
+from .models import AvoidedFood, WasteStat, Customer
 
 response={}
 
 class Profile(APIView):
-    authentication_classes = []
-    permission_classes = []
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     def get(self, request, format=None):
         user = request.user
         customer = Customer.objects.get(user = user)
         dict = {
         'Username': customer.user.username,
         'Biography': customer.bio,
-        'Photo': customer.photo
+        'Photo': customer.photo.url,
         }
         return JsonResponse(dict)
     def post(self, request, format=None):
