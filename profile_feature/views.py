900c28645ae8b25aa9611311516526de80c370a7
TheBloodCow <adiguna.amal@yahoo.com>
2021-08-14 21:09:58 +0700

    Revamped to use API

--- profile_feature/views.py
+++ profile_feature/views.py
@@ -1,21 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from .models import profile, wasteStats
-from .forms import changeBioForm
+from .models import AvoidedFood, WasteStat, Customer
+from django.http import JsonResponse
 
 response={}
 
-def changeBioForm(request):
-    response["changeBioForm"] = changeBioForm()
-    return render(request, "changeBioForm", response)
-
-def replaceBio(request):
-    form = changeBioForm(request.POST or None)
-    if request.method == "POST" and form.is_valid():
-        #temp needs to be changed to view that directs to profile html
-        temp = profile.objects.all().delete()
-        response["Bio"] = request.POST["Bio"]
-        project = profile(Status=response["Bio"])
-        project.save()
-        return HttpResponseRedirect(reverse("<Change to view that directs to profile html>"))
-    else:
-        return HttpResponseRedirect("/")
+class Profile(APIView):
+    authentication_classes = []
+    permission_classes = []
+    def get(self, request, format=None):
+        user = request.user
+        customer = Customer.objects.get(user = user)
+        dict = {
+        'Username': customer.user.username,
+        'Biography': customer.bio,
+        'Photo': customer.photo
+        }
+        return JsonResponse(dict)
+    def post(self, request, format=None):
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
