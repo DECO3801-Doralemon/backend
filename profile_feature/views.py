d37c2f32c3141fe5944ac8f04fcb00e6b8e32867
Figo Muhammad <figomuhammad9@protonmail.com>
2021-08-24 17:01:13 +0700

    Tidy up code

--- profile_feature/views.py
+++ profile_feature/views.py
@@ -10,36 +10,42 @@ from django.http import JsonResponse
 
 from .models import WasteStat, Customer
 
-response={}
+response = {}
+
 
 class Profile(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
     def get(self, request, format=None):
         user = request.user
-        customer = Customer.objects.get(user = user)
+        customer = Customer.objects.get(user=user)
         return JsonResponse({
-        'First Name': customer.user.first_name,
-        'Last Name': customer.user.last_name,
-        'Username': customer.user.username,
-        'Email' : customer.user.email,
-        'Biography': customer.bio,
-        'Photo': customer.photo.url,
+            'First Name': customer.user.first_name,
+            'Last Name': customer.user.last_name,
+            'Username': customer.user.username,
+            'Email': customer.user.email,
+            'Biography': customer.bio,
+            'Photo': customer.photo.url,
         })
+
     def post(self, request, format=None):
         user = request.user
-        customer = Customer.objects.get(user = user)
-        serializer = EditSerializer(customer, data = request.data)
+        customer = Customer.objects.get(user=user)
+        serializer = EditSerializer(customer, data=request.data)
         if serializer.is_valid():
             customer = serializer.save()
             return HttpResponse(status=200)
         return JsonResponse(serializer.errors, status=400)
+
+
 class EditPassword(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
     def post(self, request, format=None):
         user = request.user
-        serializer = EditPasswordSerializer(user, data = request.data)
+        serializer = EditPasswordSerializer(user, data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             return HttpResponse(status=200)
