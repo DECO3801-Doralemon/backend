2187e83e0a8beb70db6c77e63b484e029087d311
Figo Muhammad <figomuhammad9@protonmail.com>
2021-08-29 13:49:58 +0700

    Tidy up profile and recipe apps

--- profile_feature/models.py
+++ profile_feature/models.py
@@ -3,11 +3,18 @@ from django.contrib.auth.models import User
 from django.db.models.signals import m2m_changed
 from django.core.exceptions import ValidationError
 
+
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=100, blank=True, null=True)
-    photo = models.ImageField(upload_to='profilePic', default='profilePic/GDR.PNG', blank=True, null=True)
+    photo = models.ImageField(
+        upload_to='profilePic', default='profilePic/GDR.PNG', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+
 class WasteStat(models.Model):
-    totalFood =  models.IntegerField(default = 0)
-    wastedFood = models.IntegerField(default = 0)
-    foodEfficiency = models.IntegerField(default = 0)
+    totalFood = models.IntegerField(default=0)
+    wastedFood = models.IntegerField(default=0)
+    foodEfficiency = models.IntegerField(default=0)
