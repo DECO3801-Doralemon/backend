cec56fe9cd60fb5471c3e0e1dfecf7d47c031ed7
Figo Muhammad <figomuhammad9@protonmail.com>
2021-08-30 16:27:30 +0700

    Tidy up code

--- profile_feature/serializers.py
+++ profile_feature/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from profile_feature.models import Customer
 
+
 class EditSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=100)
     last_name = serializers.CharField(max_length=100)
@@ -10,17 +11,21 @@ class EditSerializer(serializers.Serializer):
     photo = serializers.ImageField(default='profilePic/GDR.PNG')
 
     def update(self, instance, validated_data):
-        instance.user.first_name = validated_data.get('first_name', instance.user.first_name)
-        instance.user.last_name = validated_data.get('last_name', instance.user.last_name)
+        instance.user.first_name = validated_data.get(
+            'first_name', instance.user.first_name)
+        instance.user.last_name = validated_data.get(
+            'last_name', instance.user.last_name)
         instance.user.email = validated_data.get('email', instance.user.email)
         instance.bio = validated_data.get('bio', instance.bio)
         instance.photo = validated_data.get('photo', instance.photo)
         instance.save()
         return instance
 
+
 class EditPasswordSerializer(serializers.Serializer):
     oldPassword = serializers.CharField(max_length=100)
     password = serializers.CharField(max_length=100)
+
     def update(self, instance, validated_data):
         if user.check_password(oldPassword):
             if user.check_password(password):
