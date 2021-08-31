d93e1567762638063bb1056adb0eded9ff2f6e7c
Figo Muhammad <figomuhammad9@protonmail.com>
2021-08-31 14:36:06 +0700

    Add user identifier in views

--- shopping/views.py
+++ shopping/views.py
@@ -1,9 +1,15 @@
+from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from profile_feature.models import Customer
 
 
 class ShoppingView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, year, month, day, format=None):
+    def get(self, request, format=None):
+        user = request.user
+        customer = Customer.objects.get(user=user)
+
+        
