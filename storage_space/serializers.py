903503d7c01e35f93c46dc821ee05d350778d831
Figo Muhammad <figomuhammad9@protonmail.com>
2021-09-12 21:51:42 +0700

    Delete walrus operator

--- storage_space/serializers.py
+++ storage_space/serializers.py
@@ -5,7 +5,8 @@ class EditStorageSerializer(serializers.Serializer):
     kg = serializers.IntegerField(min_value=0)
 
     def update(self, instance, validated_data):
-        if (kg := validated_data['kg']) > 0:
+        kg = validated_data['kg']
+        if kg > 0:
             instance.kg = kg
             instance.save()
         else:
