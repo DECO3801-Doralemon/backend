3f37da617e163593a2e88584fb1beaf623ac09b7
TheBloodCow <adiguna.amal@yahoo.com>
2021-09-12 21:24:16 +0700

    New model

--- community/models.py
+++ community/models.py
@@ -5,6 +5,8 @@ from recipes_and_ingredients.models import Recipe
 class OnlineRecipe(models.Model):
     recipe= models.ForeignKey(Recipe, on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
+    photo = models.ImageField(
+    upload_to='profile_photo', default='profile_photo/test_photo.PNG', blank=True, null=True)
 
     def __str__(self) -> str:
         return f"{self.recipe}"
