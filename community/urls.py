d773989f76980d8594a10832e640cbee503b5b8f
Figo Muhammad <figomuhammad9@protonmail.com>
2021-10-26 18:37:02 +0700

    Add url for POST method of community recipe

--- community/urls.py
+++ community/urls.py
@@ -3,5 +3,6 @@ from .views import CommunityView, MassCommunityView
 
 urlpatterns = [
     path('recipe/<int:community_recipe_id>', CommunityView.as_view(), name="recipe"),
+    path('recipe', CommunityView.as_view(), name="recipe"),
     path('', MassCommunityView.as_view(), name="feed"),
 ]
